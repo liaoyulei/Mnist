d4154006b59d0835e65fca58976e722aba7fd8a9
liaoyulei <liao.yu.lei@163.com>
2017-10-09 16:52:26 +0800

    finish forward pass

--- example.cpp
+++ example.cpp
@@ -45,14 +45,14 @@ public:
 class ReLU{
 public:
     int n;
+    FLTARY top;
     ReLU(int output) {
         n = output;
+        top.resize(output);
     }
-    void forward_pass(FLTARY &bottom) {
+    void forward_pass(FLTARY bottom) {
         for(int i = 0; i < n; ++i) {
-            if(bottom[i] < 0) {
-                bottom[i] = 0;
-            }
+            top[i] = bottom[i] > 0 ? bottom[i] : 0;
         }
     }
 };
@@ -60,21 +60,23 @@ public:
 class SoftmaxWithLoss{
 public:
     int n;
+    FLTARY top;
     SoftmaxWithLoss(int output) {
         n = output;
+        top.resize(output);
     }
-    void forward_pass(FLTARY &bottom) {
+    void forward_pass(FLTARY bottom) {
         double ymax = *std::max_element(bottom.begin(), bottom.end()), sum = 0;
         for(int i = 0; i < n; ++i) {
-            bottom[i] -= ymax;
-            bottom[i] = exp(bottom[i]);
-            sum += bottom[i];
+            top[i] = exp(bottom[i] - ymax);
+            sum += top[i];
         }
         for(int i = 0; i < n; ++i) {
-            bottom[i] /= sum;
+            top[i] /= sum;
         }
     }
-    void back_pass(){
+    void back_pass() {
+
     }
 };
 
@@ -108,7 +110,7 @@ void LoadData(_IS &inStream, int *pImgRows, int *pImgCols,
 	}
 }
 
-int main(int nArgCnt, char *ppArgs[]) {
+int main() {
     int nImgRows, nImgCols, batch = 1000;
     double base_lr = 0.1;
 	std::vector<FLTARY> trainImgs, testImgs;
@@ -117,21 +119,23 @@ int main(int nArgCnt, char *ppArgs[]) {
 	LoadData(ifs, &nImgRows, &nImgCols, trainImgs, trainLabels, testImgs);
 //	LoadData(std::cin, &nImgRows, &nImgCols, trainImgs, trainLabels, testImgs);
 	ifs.close();
-    InnerProduct fc1(nImgRows * nImgCols, 500), fc2(500, 10);
+//    InnerProduct fc1(nImgRows * nImgCols, 500), fc2(500, 10);
+    InnerProduct fc(nImgRows * nImgCols, 10);
     ReLU relu(500);
     SoftmaxWithLoss loss(10);
-    for(int i = 0; i < 10000; ++i) {//max_iter
-        if (!(i % 3000)) {//sttesize
+    for(int i = 0; i < 100; ++i) {//max_iter
+        if (!(i % 300)) {//sttesize
             base_lr *= 0.1;//gammar
         }
         for(int j = 0; j < trainImgs.size(); j += batch) {
             for(int k = 0; k < batch; ++k) {
-                fc1.forward_pass(trainImgs[j + k]);
+            /*    fc1.forward_pass(trainImgs[j + k]);
                 relu.forward_pass(fc1.top);
-                fc2.forward_pass(fc1.top);
-                loss.forward_pass(fc2.top);
+                fc2.forward_pass(relu.top);
+                loss.forward_pass(fc2.top);*/
+                fc.forward_pass(trainImgs[j + k]);
+                loss.forward_pass(fc.top);
             }
-
         }
     }
 	system("pause");
